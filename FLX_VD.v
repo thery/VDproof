72cd8fe2a13c411bf3b9352dc6ffe4062e0e73e5
thery <Laurent.Thery@inria.fr>
2024-02-26 14:42:07 +0100

    starting

--- FLX_VD.v
+++ FLX_VD.v
@@ -4,7 +4,7 @@
 Require Import Reals  Psatz.
 From Flocq Require Import Core Plus_error Mult_error Relative Sterbenz Operations.
 From Flocq Require Import  Round.
-Require Import mathcomp.ssreflect.ssreflect.
+Require Import mathcomp.ssreflect.ssreflect Rmore.
 
 Set Implicit Arguments.
 
@@ -25,6 +25,12 @@ Local Notation RND := (round beta fexp rnd).
 
 Inductive dwR := DWR (xh : R) (xl : R).
 
+Definition opp_DWR (d : dwR) := 
+ let: DWR xh xl := d in DWR (- xh) (- xl).
+
+Definition scale_DWR (s : R) (d : dwR) := 
+ let: DWR xh xl := d in DWR (s * xh) (s * xl).
+
 Open Scope R_scope.
 
 Definition split (x : R) (s : Z) :=
@@ -34,6 +40,17 @@ Definition split (x : R) (s : Z) :=
   let xh := RND (g + d) in 
   let xl := RND (x - xh) in 
   DWR xh xl.
+
+Lemma split_0 s : split 0 s = DWR 0 0.
+Proof. by rewrite /split !(round_0, Rsimp01). Qed.
+
+Lemma split_opp x s : 
+  (forall x, RND (- x) = - RND x) -> split (- x) s = opp_DWR (split x s).
+Proof.
+move=> oppR; rewrite /Rminus /split.
+have -> : (pow s + 1) * - x = - ((pow s + 1) * x) by lra.
+by rewrite /Rminus !(oppR, (fun x y => sym_equal (Ropp_plus_distr x y))).
+Qed.
 
 End Main.
 
