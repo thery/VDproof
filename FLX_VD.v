11b170de69008f084520cea55fedd590d54a42e6
thery <Laurent.Thery@inria.fr>
2024-02-27 00:45:31 +0100

    sign done

--- FLX_VD.v
+++ FLX_VD.v
@@ -38,6 +38,11 @@ Definition split (x : R) (s : Z) :=
   let xl := RND (x - xh) in 
   DWR xh xl.
 
+Context ( valid_rnd : Valid_rnd rnd ).
+
+Lemma split_0 s : split 0 s = DWR 0 0.
+Proof. by rewrite /split !(round_0, Rsimp01). Qed.
+
 End split.
 
 Lemma split_opp rnd fexp x s : 
@@ -85,9 +90,6 @@ Local Notation mant := (scaled_mantissa beta fexp).
 Local Notation RND := (round beta fexp rnd).
 Local Notation split := (split fexp rnd).
 
-Lemma split_0 s : split 0 s = DWR 0 0.
-Proof. by rewrite /split !(round_0, Rsimp01). Qed.
-
 Theorem cexp_bpow_flx x e (xne0 : x <> R0) : ce (pow e * x) = (ce x + e)%Z.
 Proof. by rewrite /cexp Rmult_comm mag_mult_bpow // /fexp; lia. Qed.
 
@@ -118,14 +120,27 @@ Hypothesis  basic_rnd : [\/ rnd = Ztrunc,
 Lemma split_sum x s : let: DWR xh xl := split x s in x = xh + xl.
 Proof.
 move: rnd valid_rnd basic_rnd.
-wlog : / 0 < x => [HR rnd1 Vrnd1| xP rnd1 Vrnd1 _].
+wlog : x / 0 < x => [IH rnd1 Vrnd1| xP rnd1 Vrnd1 Brnd1].
   have [->|xP|xN]: [\/ x = 0, 0 < x | x < 0].
   - case: (Rle_dec x 0) => ?; last by apply: Or32; lra.
     by case: (Req_dec x 0) => ?; [apply: Or31| apply: Or33; lra].
-  - rewrite split_0.
-
-  lra.
-  admit.
+  - by rewrite split_0 //; lra.
+  - by apply: IH.
+  have xNN : 0 < - x by lra.
+  case => rE; rewrite rE.
+  - suff : let 'DWR xh xl := FLX_VD.split fexp Ztrunc (- x) s in - x = xh + xl.
+      by rewrite split_ZR_opp; case: FLX_VD.split => /= xh xl; lra.
+    by apply: IH => //; apply: Or41.
+  - suff : let 'DWR xh xl := FLX_VD.split fexp Zaway (- x) s in - x = xh + xl.
+      by rewrite split_AW_opp; case: FLX_VD.split => /= xh xl; lra.
+    by apply: IH => //; apply: Or42.
+  - suff : let 'DWR xh xl := FLX_VD.split fexp Zceil (- x) s in - x = xh + xl.
+      by rewrite split_UP_opp; case: FLX_VD.split => /= xh xl; lra.
+    by apply: IH => //; apply: Or44.
+  suff : let 'DWR xh xl := FLX_VD.split fexp Zfloor (- x) s in - x = xh + xl.
+    by rewrite split_DN_opp; case: FLX_VD.split => /= xh xl; lra.
+  by apply: IH => //; apply: Or43.
+Qed.
 
 End Main.
 
