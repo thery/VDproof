8abb043b835064784d7a8f50436274e5d01bae33
thery <Laurent.Thery@inria.fr>
2024-02-27 02:08:50 +0100

    simplet

--- FLX_VD.v
+++ FLX_VD.v
@@ -46,33 +46,18 @@ Proof. by rewrite /split !(round_0, Rsimp01). Qed.
 End split.
 
 Lemma split_opp rnd fexp x s : 
-  (forall x, round beta fexp rnd (- x) = - round beta fexp rnd x) -> 
-  split fexp rnd (- x) s = opp_DWR (split fexp rnd x s).
-Proof.
-move=> oppR; rewrite /Rminus /split.
-have -> : (bpow beta s + 1) * - x = - ((bpow beta s + 1) * x) by lra.
-by rewrite /Rminus !(oppR, =^~ Ropp_plus_distr).
-Qed.
-
-Lemma split_ZR_opp fexp x s : 
-  split fexp Ztrunc (- x) s = opp_DWR (split fexp Ztrunc x s).  
-Proof. by apply/split_opp/round_ZR_opp. Qed.
-
-Lemma split_AW_opp fexp x s : 
-  split fexp Zaway (- x) s = opp_DWR (split fexp Zaway x s).  
-Proof. by apply/split_opp/round_AW_opp. Qed.
-
-Lemma split_UP_opp fexp x s : 
-  split fexp Zceil (- x) s = opp_DWR (split fexp Zfloor x s).  
+  split fexp rnd (- x) s = opp_DWR (split fexp (Zrnd_opp rnd) x s).
 Proof.
 rewrite /Rminus /split.
 have -> : (bpow beta s + 1) * - x = - ((bpow beta s + 1) * x) by lra.
-by rewrite /Rminus !(round_UP_opp, =^~ Ropp_plus_distr).
+by rewrite /Rminus !(round_opp, =^~ Ropp_plus_distr).
 Qed.
 
-Lemma split_DN_opp fexp x s : 
-  split fexp Zfloor (- x) s = opp_DWR (split fexp Zceil x s).  
-Proof. by rewrite -[in RHS](Ropp_involutive x) split_UP_opp opp_DWR_K. Qed.
+Lemma split_oppr rnd fexp x s : 
+  split fexp (Zrnd_opp rnd) (- x) s = opp_DWR (split fexp rnd x s).
+Proof.
+by rewrite -[in RHS](Ropp_involutive x) [in RHS]split_opp opp_DWR_K.
+Qed.
 
 Section Main.
 
@@ -89,6 +74,7 @@ Local Notation ce := (cexp beta fexp).
 Local Notation mant := (scaled_mantissa beta fexp).
 Local Notation RND := (round beta fexp rnd).
 Local Notation split := (split fexp rnd).
+Local Notation splitr := (split fexp (Zrnd_opp rnd)).
 
 Theorem cexp_bpow_flx x e (xne0 : x <> R0) : ce (pow e * x) = (ce x + e)%Z.
 Proof. by rewrite /cexp Rmult_comm mag_mult_bpow // /fexp; lia. Qed.
@@ -113,33 +99,20 @@ have -> : (pow s + 1) * (pow t * x) = pow t * ((pow s + 1) * x) by lra.
 by rewrite !(round_bpow_flx, =^~ Rmult_minus_distr_l, =^~ Rmult_plus_distr_l).
 Qed.
 
-Hypothesis  basic_rnd : [\/ rnd = Ztrunc,
-                            rnd = Zaway,
-                            rnd = Zfloor | rnd = Zceil].
- 
 Lemma split_sum x s : let: DWR xh xl := split x s in x = xh + xl.
 Proof.
-move: rnd valid_rnd basic_rnd.
-wlog : x / 0 < x => [IH rnd1 Vrnd1| xP rnd1 Vrnd1 Brnd1].
+move: rnd valid_rnd.
+wlog : x / 0 < x => [IH rnd1 Vrnd1| xP rnd1 Vrnd1].
   have [->|xP|xN]: [\/ x = 0, 0 < x | x < 0].
   - case: (Rle_dec x 0) => ?; last by apply: Or32; lra.
     by case: (Req_dec x 0) => ?; [apply: Or31| apply: Or33; lra].
   - by rewrite split_0 //; lra.
   - by apply: IH.
   have xNN : 0 < - x by lra.
-  case => rE; rewrite rE.
-  - suff : let 'DWR xh xl := FLX_VD.split fexp Ztrunc (- x) s in - x = xh + xl.
-      by rewrite split_ZR_opp; case: FLX_VD.split => /= xh xl; lra.
-    by apply: IH => //; apply: Or41.
-  - suff : let 'DWR xh xl := FLX_VD.split fexp Zaway (- x) s in - x = xh + xl.
-      by rewrite split_AW_opp; case: FLX_VD.split => /= xh xl; lra.
-    by apply: IH => //; apply: Or42.
-  - suff : let 'DWR xh xl := FLX_VD.split fexp Zceil (- x) s in - x = xh + xl.
-      by rewrite split_UP_opp; case: FLX_VD.split => /= xh xl; lra.
-    by apply: IH => //; apply: Or44.
-  suff : let 'DWR xh xl := FLX_VD.split fexp Zfloor (- x) s in - x = xh + xl.
-    by rewrite split_DN_opp; case: FLX_VD.split => /= xh xl; lra.
-  by apply: IH => //; apply: Or43.
+  suff : let 'DWR xh xl := FLX_VD.split fexp (Zrnd_opp rnd1) (- x) s 
+         in - x = xh + xl.
+    by rewrite split_oppr; case: FLX_VD.split => /= xh xl; lra.
+  by apply: IH.
 Qed.
 
 End Main.
